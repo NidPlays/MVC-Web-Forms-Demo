5a23ba1ae2f7f617a9944f512422c32d379e4bc1
Nid@Laptop <22031046+NidPlays@users.noreply.github.com>
2022-03-19 11:49:46 +0530

    Add project files.

---
+++ MVC Web Forms/Controllers/PatientsController.cs
@@ -0,0 +1,20 @@
+﻿using MVC_Web_Forms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Web_Forms.Controllers
+{
+    public class PatientsController : Controller
+    {
+        // GET: Patients
+        HospitalContext hc = new HospitalContext();
+        public ActionResult Index()
+        {
+            List<Patient> plist = hc.patient.ToList();
+            return View(plist);
+        }
+    }
+}
---
+++ MVC Web Forms/Models/Patient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Web_Forms.Models
+{
+    public class Patient
+    {
+        public int id { get; set; }
+        [Display(Name = "Name")]        
+        public string name { get; set; }
+        public float temp { get; set; }
+        public string allergies { get; set; }
+    }
+
+    public class HospitalContext: DbContext
+        {
+            public DbSet<Patient> patient { get; set; }
+        }
+}
